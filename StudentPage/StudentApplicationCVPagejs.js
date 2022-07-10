fdc6b5a43b3242eb970ad10c5761ac867eecce99
Echo2craft <justechojepar@gmail.com>
2022-07-10 16:58:12 +0700

    10/07/2022 Finished Student Pages, Company Pages and Added a Few CRD Pages (by OanhToNgo)

--- StudentPage/StudentApplicationCVPagejs.js
+++ StudentPage/StudentApplicationCVPagejs.js
@@ -42,47 +42,6 @@ onload = () => {
     })
     /* End of JS Tag names */
 
-    /* JS for Slider source: https://www.youtube.com/watch?v=0wvrlOyGlq0*/
-    const radiobox = document.querySelectorAll('[id^="radio"]');
-    // console.log(radiobox);
-    radiobox.forEach(radio => {
-        // console.log(typeof(parseInt(radio.id.slice(5))));
-        radio.addEventListener('click', function () {
-            return counter = parseInt(radio.id.slice(5));
-        });
-    })
-    /* End of JS Slider */
-    /* JS for Slider name: add slider tag names to SearchBox upon click */
-    document.getElementById('sponsortags1').addEventListener('click', function () {
-        if (text.value.length > 0) {
-            text.value += ' 2022';
-        } else {
-            text.value = '2022';
-        }
-    });
-    document.getElementById('sponsortags2').addEventListener('click', function () {
-        if (text.value.length > 0) {
-            text.value += ' Microsoft';
-        } else {
-            text.value = 'Microsoft';
-        }
-    });
-    document.getElementById('sponsortags3').addEventListener('click', function () {
-        if (text.value.length > 0) {
-            text.value += ' Tesla 2022';
-        } else {
-            text.value = 'Tesla 2022';
-        }
-    });
-    document.getElementById('sponsortags4').addEventListener('click', function () {
-        if (text.value.length > 0) {
-            text.value += ' Nestle 2022';
-        } else {
-            text.value = 'Nestle 2022';
-        }
-    });
-    /* End of JS Slider tag names */
-
     /* JS for Detail Button */
     // document.getElementById('internfulldetail').addEventListener('click',function(){
     //     window.location =''; //'/LoginPage/LoginPage.html';
@@ -101,7 +60,8 @@ onload = () => {
             clonedetail.removeChild(cloneinternboard.children[1].children[3]);
             clonedetail.appendChild(clonebottomdiv);
             clonedetail.insertBefore(cloneimg, clonedetail.children[0]);
-
+            
+            popup.children[0].children[1].innerHTML = e.innerHTML;
             popup.insertBefore(clonedetail, popup.children[0]);
             popup.classList.toggle('active');
         });
@@ -126,15 +86,3 @@ onload = () => {
     /* End of JS Detail Button */
 };
 /* End of JS On Page Load */
-
-/* JS for Automatic Left to Right Slider after 5s delay*/
-setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    if (document.hasFocus()) {
-        counter++;
-    }
-    if (counter > 4) {
-        counter = 1;
-    };
-}, 5000);
-/* End of JS Slider */
